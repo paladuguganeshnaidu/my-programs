6cb9ac88a124507d02e216c7ec1450a805f11282
GANESH NAIDU <1nc24cs150@ncetmail.com>
2026-05-20 14:44:28 +0530

    Lab-7 in progress

---
+++ Java/Labs/Lab7.java
@@ -0,0 +1,5 @@
+//develop a java program that emulates library system. create two packages library &  patron in library package define a book class with a provate title feed. in the patron package , implement a patron class that can barrow books demonstrate the use of the packages , access protection & class imports .ensure that the book title remains in accessable from outside the library package due to the provate access modifier. create a sceanario where a pattren allice borrows a book form the library.
+
+public class Lab7 {
+    
+}
